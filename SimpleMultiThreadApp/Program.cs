918d05e2f8daec55fdb0988f66ab2d9f11923267
ADILET <alpha.z91@mail.ru>
2019-06-13 12:26:32 +0600

    Добавьте файлы проекта.

---
+++ SimpleMultiThreadApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SimpleMultiThreadApp
+{
+    class Printer
+    {
+        private object threadLock = new object();
+        public void PrintNumbers()
+        {
+            Monitor.Enter(threadLock);
+            try
+            {
+                // Вывести информацию о потоке.
+                Console.WriteLine("-> {0} is executing PrintNumbers()",
+                Thread.CurrentThread.Name);
+                // Вывести числа.
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(1000 * r.Next(5));
+                    Console.Write("{0}, ", i);
+                }
+                Console.WriteLine();
+            }
+            finally
+            {
+                Monitor.Exit(threadLock);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Synchronizing Threads *****\n");
+            Printer p = new Printer();
+            // Создать 10 потоков, которые указывают на один
+            // и тот же метод того же самого объекта.
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] =
+                new Thread(new ThreadStart(p.PrintNumbers));
+                threads[i].Name = string.Format("Worker thread #{0}", i);
+            }
+            // Теперь запустить их все.
+            foreach (Thread t in threads)
+                t.Start();
+            Console.ReadLine();
+        }
+    }
+}
